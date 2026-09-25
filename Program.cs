fc8f22ad8aabbe1877566eeee53e3a7e9f77c08c
MarkoSebass <ms.orihuelac@gmail.com>
2026-09-25 11:54:31 -0500

    CINTA

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using MsPrueba.Services.Notifications;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
@@ -20,7 +19,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -28,7 +26,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,12 +1,11 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.querySelectorAll(".task-toast").forEach((toast) => {
+  const hideToast = () => {
+    toast.classList.add("is-hiding");
+    window.setTimeout(() => toast.remove(), 200);
+  };
 
-document.querySelectorAll('.task-toast').forEach((toast) => {
-	const hideToast = () => {
-		toast.classList.add('is-hiding');
-		window.setTimeout(() => toast.remove(), 200);
-	};
-
-	toast.querySelector('.task-toast-close')?.addEventListener('click', hideToast);
-	window.setTimeout(hideToast, 1500);
+  toast
+    .querySelector(".task-toast-close")
+    ?.addEventListener("click", hideToast);
+  window.setTimeout(hideToast, 1500);
 });
